a8db11795a47c0f8f268ed408d6f4ddf77413fe7
Cullen Mooney <cullen@protonmail.com>
2018-01-15 21:29:17 -0600

    optionals

--- Classes and Objects/selfdriving.swift
+++ Classes and Objects/selfdriving.swift
@@ -4,6 +4,12 @@
 //  Copyright © 2018 Cullen Mooney. All rights reserved.
 //
 
+// OPTIONALS
+// changes data type to an optional string, or optional integer etc.
+// could contain a value or could contain nothing - nil
+// ! after name of variable is force unwrapping - manual override, unsafe
+// optional binding - creating a new constant and only printing it when there's a value
+
 import Foundation
 
 // SUBCLASS VS SUPERCLASS
@@ -11,14 +17,20 @@ import Foundation
 // self driving car is a subclass of the super car
 class SelfDrivingCar : Car {
    // property that has the destination address
-    var destination : String = "1 Infinite Loop"
+    // var destination : String = "1 Infinite Loop"
+    
+    // created an optional - might contain a value, might not
+    var destination : String?
     
     // by inheriting from the car class, we already have a method called 'drive' so it needs to be overrided
     override func drive() {
         // super refers to the superclass 'Car'
         super.drive()
         
-        // now going to put our own modifications for the drive method
-        print("driving towards " + destination)
+        if let userSetDestination = destination {
+            // using let to create new constant, value is set to equal our destination optional
+            // this is optional binding - safer b/c will only execute when it's safe
+            print("driving towards " + userSetDestination)
+        }
     }
 }
